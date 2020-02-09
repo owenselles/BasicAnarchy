4e83914662a10cd0054c8ca3c7b4bdc005ee1835
Owen <owenselles@icloud.com>
2020-02-09 16:20:28 +0100

    1.0.3

--- src/main/java/com/owenselles/basicanarchy/Events/OnPlayerJoin.java
+++ src/main/java/com/owenselles/basicanarchy/Events/OnPlayerJoin.java
@@ -12,7 +12,7 @@ public class OnPlayerJoin implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
             Player player = event.getPlayer();
 
-            player.sendMessage(Color.add("&6 Welcome to Anarchy! &fwhere teams are formed, empires build and bases raided. " +
+            player.sendMessage(Color.add("&6 Welcome to Anarchy! &fwhere teams are formed, empires built and bases raided. " +
                     "In this ever-changing world you are free to do whatever your heart desires."));
             player.sendMessage(Color.add(""));
             player.sendMessage(Color.add("&f The only rule we follow is &cNO HACKED CLIENTS&f. This will get you banned."));
